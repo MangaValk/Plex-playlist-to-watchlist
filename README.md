9b7df9295b71f47fb8fb7c50c4898743239b112d
Unknown <patrickvalkmanga@hotmail.com>
2022-10-15 16:46:29 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Plex-playlist-to-watchlist
+Move shows and movies from plex playlist to watchlist in bulk
---
+++ main.py
@@ -0,0 +1,75 @@
+#  move shows and movies from plex playlist to watchlist in bulk
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+
+import urllib
+import requests
+
+def getPlaylistId(plexUrl, plexToken):
+    playlistsLink = "%s/playlists?X-Plex-Token=%s" % (plexUrl, plexToken)
+    f = urllib.urlopen(playlistsLink)
+    parsed_html = BeautifulSoup(f.read(), 'xml')
+    playlists = {}
+    for playlist in parsed_html.find_all("Playlist"):
+        playlists[int(playlist.get("ratingKey"))] = playlist.get("title")
+    while 1:
+        for playlistKey in playlists.keys():
+            val = playlists[playlistKey]
+            print("Key", playlistKey, 'value', val)
+
+        try:
+            selectedPlaylistKey = int(input("enter source playlist key:"))
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+
+        if selectedPlaylistKey in playlists.keys():
+            break
+
+        print "invalid playlist key!"
+    return selectedPlaylistKey
+
+def getAllShowIdentifiersFromPlaylist(plexUrl, plexToken, playlistId):
+    playlistLink = "%s/playlists/%s/items?X-Plex-Token=%s" % (plexUrl, playlistId, plexToken)
+    f = urllib.urlopen(playlistLink)
+    parsed_html = BeautifulSoup(f.read(), 'xml')
+    playlistShowIdentifiers = [];
+    for videos in parsed_html.find_all("Video"):
+        try:
+            playlistShowIdentifiers.append(videos.get("grandparentGuid").replace("plex://show/", ""))
+        except:
+            pass
+        try:
+            playlistShowIdentifiers.append(videos.get("guid").replace("plex://movie/", ""))
+        except:
+            pass
+
+    # make distinct
+    return list(set(playlistShowIdentifiers))
+
+
+plexUrl = input("please enter plex url (example: 127.0.0.1:32400):")
+
+plexToken = input("please enter plex token (see: https://www.plexopedia.com/plex-media-server/general/plex-token/):")
+
+playlistId = getPlaylistId(plexUrl, plexToken)
+
+playlistShowIdentifiers = getAllShowIdentifiersFromPlaylist(plexUrl, plexToken, playlistId)
+
+print playlistShowIdentifiers
+
+#  https://curlconverter.com/
+headers = {
+    'x-plex-token': ('%s' % plexToken),
+}
+
+for result in playlistShowIdentifiers:
+    params = {
+        'ratingKey': result
+    }
+
+    response = requests.put('https://metadata.provider.plex.tv/actions/addToWatchlist', params=params, headers=headers)
+
+    print(response)
